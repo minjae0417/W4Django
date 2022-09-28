64c598641acd5404d9d722d496be015f7cbb0f95
JG-C3 <jgc3django@gmail.com>
2022-09-28 21:55:12 +0900

    Django W4

---
+++ functions/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index/', views.index),
+    path('image/', views.image),
+    path('lucky/', views.lucky),
+    path('birthday/', views.birthday),
+    path('menu/', views.menu),
+]
